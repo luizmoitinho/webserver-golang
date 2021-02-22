33bc35a585149e4e9034ce62f2d8f7635d2b8079
luiz carlos costa moitinho <luiz.moitinho@supplylabs.com.br>
2021-02-22 12:51:47 -0300

    feat - create books and route get all

--- main.go
+++ main.go
@@ -3,15 +3,48 @@ package main
 import (
 	"fmt"
 	"net/http"
+	"encoding/json"
 )
+
+type Editora struct{
+	Id int
+	Nome string
+	ISBN int
+}
+type Book struct {
+	Id int
+	Titulo string
+	Autor string
+	//Editora Editora
+}
+
+var Books []Book = []Book{
+	Book{
+		Id:1,
+		Titulo: "Poder do Hábito",
+		Autor: "Charles Duhing",
+	},
+	Book{
+		Id:2,
+		Titulo: "Dom Casmurro",
+		Autor: "Machado de Assis",
+	},
+
+}
 
 func  healthCheckServer(w http.ResponseWriter, r * http.Request){
 	fmt.Fprintf(w, "Conexão estabelecida com sucesso.")
 }
 
+func getAllBooks(w http.ResponseWriter, r * http.Request){
+	encoder :=  json.NewEncoder(w)
+	encoder.Encode(Books)
+}
 
 func serverConfig(){
+
 	http.HandleFunc("/", healthCheckServer );
+	http.HandleFunc("/books", getAllBooks );
 
 	err := http.ListenAndServe(":1337", nil); // DefaultServerMux
 	if err != nil{
